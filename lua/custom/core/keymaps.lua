5aaa1205990728bac61cc818958c662eeb85cf83
Leonardo Barrocal <leohb1501@gmail.com>
2024-08-31 02:45:21 -0300

    feat: add new vim motion

--- lua/custom/core/keymaps.lua
+++ lua/custom/core/keymaps.lua
@@ -6,3 +6,5 @@ vim.keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh 
 
 vim.keymap.set('n', '<leader>o', 'o<ESC>', { desc = 'Insert new line below' })
 vim.keymap.set('n', '<leader>O', 'O<ESC>', { desc = 'Insert new line above' })
+
+vim.keymap.set('i', 'jj', '<Esc>', { desc = 'Esc' })
